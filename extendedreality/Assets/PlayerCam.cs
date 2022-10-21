358d606dc74f11121731cb9114a9f4bc58160983
CatalinaG-R <116341938+CatalinaG-R@users.noreply.github.com>
2022-10-21 12:14:36 +0200

    movement

--- extendedreality/Assets/PlayerCam.cs
+++ extendedreality/Assets/PlayerCam.cs
@@ -22,8 +22,8 @@ public class PlayerCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = input.GetAxisRaw('Mouse X') * Time.deltaTime * sensX;
-        float mouseY = input.GetAxisRaw('Mouse Y') * Time.deltaTime * sensY;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         yRotation += mouseX;
         xRotation -= mouseY;
